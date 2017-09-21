b2cf668c459fc3c1cbec1dba77f1f1fb734ec35b
Chris Poole <chrispoole@uk.ibm.com>
2017-09-21 17:04:25 +0100

    Add SSL-config file
    Change-Id: Ie1229216f12b8d62c3db7eb1885e0e2e04237703
    Signed-off-by: Chris Poole <chrispoole@uk.ibm.com>

---
+++ packages/fabric-rest/server/ssl-config.js
@@ -0,0 +1,5 @@
+var path = require('path');
+var fs = require("fs");
+
+exports.privateKey = fs.readFileSync(path.join(__dirname, './private/privatekey.pem')).toString();
+exports.certificate = fs.readFileSync(path.join(__dirname, './private/certificate.pem')).toString();
