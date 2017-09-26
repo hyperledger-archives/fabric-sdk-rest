62e8d7ace97a3b0272f24dd900a069929a166065
Chris Poole <chrispoole@uk.ibm.com>
2017-09-26 13:30:18 +0100

    Add ldapjs server for testing
    Change-Id: I14d355f7bedca8311a9c5f364480eb211730b116
    Signed-off-by: Chris Poole <chrispoole@uk.ibm.com>

---
+++ tests/authentication/authentication.js
@@ -0,0 +1,17 @@
+//
+// Copyright IBM Corp. All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+// ldapsearch -x -D "dc=example,dc=org" -W -H ldap://0.0.0.0:1389 -b "dc=example,dc=org" -s sub 'uid=alice'
+
+'use strict';
+
+const port=1389;
+const ldapserver = require('./ldapserver');
+const password='secret';
+
+console.log('starting server');
+ldapserver.start(port, password);
+console.log('server started, listening on port ' + port);
