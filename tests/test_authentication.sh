7bb4a3b8a157065dbc466e1cf92c88c359b14a02
Chris Poole <chrispoole@uk.ibm.com>
2017-10-02 18:15:27 +0100

    Improve simple LDAP auth test
    Part of FAB-5909 work.
    
    Change-Id: I815803faa259152652b0d484d8d99779528fa32c
    Signed-off-by: Chris Poole <chrispoole@uk.ibm.com>

--- tests/test_authentication.sh
+++ tests/test_authentication.sh
@@ -29,24 +29,46 @@
 providers_file="../packages/fabric-rest/server/providers.json"
 
 node ./authentication/authentication.js &
+ldapjs_pid=$!
 sleep 3 # Just to be sure
+
+if [[ -f cookies.txt ]]; then
+    rm cookies.txt
+fi
 
 if [[ ! -f "$providers_file" ]]; then
     cp "{providers_file}.template" "$providers_file"
 fi
 
-# Get the channels, which should fail
-curl -X GET --header 'Accept: application/json' 'http://0.0.0.0:3000/api/fabric/1_0/channels' | grep Error
+# Get the channels, which should fail (JSON payload, error.statusCode=401,
+# error.message='Authorization Required')
+curl -X GET -H 'Accept: application/json' 'http://0.0.0.0:3000/api/fabric/1_0/channels' | grep 'Authorization Required'
 if [[ ! $? -eq 0 ]]; then # Error response was found
-    printf "Expected "
+    printf "Expected to find not-auth error, but didn't"
 fi
 
 # Authenticate with an incorrect password, which should fail
-curl -H 'Content-Type: application/x-www-form-urlencoded' -d 'username=alice&password=wrongsecret' 'http://0.0.0.0:3000/auth/ldap'
+curl -H 'Content-Type: application/x-www-form-urlencoded' -d 'username=alice&password=wrongsecret' 'http://0.0.0.0:3000/auth/ldap' | grep 'Redirecting to /failure'
+if [[ ! $? -eq 0 ]]; then # Error response was found
+    printf "Expected to fail auth"
+fi
 
 # Get the channels, which should fail
+curl -b cookies.txt -X GET --header 'Accept: application/json' 'http://0.0.0.0:3000/api/fabric/1_0/channels' | grep 'Authorization Required'
+if [[ ! $? -eq 0 ]]; then # Error response was found
+    printf "Expected to find not-auth error, but didn't"
+fi
 
 # Authenticate with a correct password, which should pass
-curl -H 'Content-Type: application/x-www-form-urlencoded' -d 'username=alice&password=secret' 'http://0.0.0.0:3000/auth/ldap'
+curl -c cookies.txt -H 'Content-Type: application/x-www-form-urlencoded' -d 'username=alice&password=secret' 'http://0.0.0.0:3000/auth/ldap' | grep 'Redirecting to /explorer'
+if [[ ! $? -eq 0 ]]; then # Error response was found
+    printf "Expected to succeed in auth"
+fi
 
 # Get the channels, which should now work
+curl -b cookies.txt -X GET --header 'Accept: application/json' 'http://0.0.0.0:3000/api/fabric/1_0/channels' | grep mychannel
+if [[ ! $? -eq 0 ]]; then # Error response was found
+    printf "Expected to find channel name"
+fi
+
+kill $ldapjs_pid
